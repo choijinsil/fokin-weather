a3d180a1260ad0dea82c21bd2622672324497a78
choijinsil <jinsil1106@gmail.com>
2019-10-23 19:52:23 +0900

    에러 와장창 Cannot read property 'iconName' of undefined Error boundaries should implement getDerivedStateFromError() Can't perform a React state update on an unmounted component

--- Weather.js
+++ Weather.js
@@ -1,22 +1,106 @@
 import React from "react";
-import {View,Text,StyleSheet} from "react-native";
+import {View,Text,StyleSheet, StatusBar} from "react-native";
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import { white } from "ansi-colors";
 
-export default function Weather({temp}){
+const weatherOptions = {
+    Haze: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"]
+        
+    },
+    Thunderstorm: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0","#D39D38"]
+    },
+    Drizzle: {
+       iconName: "weather-hail",
+        gradient: ["#4DA0B0","#D39D38"]
+    },
+    Rain: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0","#D39D38"]
+    },
+    Snow: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0","#D39D38"]
+    },
+    Atmosphere: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE","#66A6FF"]
+    },
+    Clear: {
+       iconName: "weather-sunny",
+        gradient: ["#FF7300","#FEF253"]
+    },
+    Clouds: {
+        iconName: "weather-cloudy",
+        gradient: ["#D7D2CC","#304352"]
+    },
+    Mist: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0","#D39D38"]
+    },
+    Dust: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0","#D39D38"]
+    }
+
+};
+
+export default function Weather({temp,condition}) {
     return (
-    <View style={styles.container}>
-    <Text>{temp}</Text>
-    </View>
+
+        <LinearGradient
+            colors={weatherOptions["Clouds"].gradient}
+            style={styles.container}> 
+            <StatusBar barStyle="light-content"/>
+            <View style={styles.halfContainer}>
+
+                <MaterialCommunityIcons 
+                    size={96} 
+                    name={weatherOptions[condition].iconName}  
+                    color="white"/>
+                <Text style={styles.temp}>{temp}도</Text>
+            </View>
+            <View style={styles.halfContainer}/>
+        </LinearGradient>
+
     );
 }
 
-Weather.propTypes={
-    temp:PropTypes.number.isRequired
+Weather.propTypes = {
+    temp: PropTypes.number.isRequired,
+    condition: PropTypes
+        .oneOf([
+            "Thunderstorm",
+            "Drizzle",
+            "Rain",
+            "Snow",
+            "Atmosphere",
+            "Clear",
+            "Clouds",
+            "Haze",
+            "Mist",
+            "Dust"
+        ])
+        .isRequired
 };
 
-const styles=StyleSheet.create({
-    container:{
-        flex:1,
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    temp:{
+        fontSize:42,
+        color:"white"
+    },
+    halfContainer: {
+        flex: 1,
         justifyContent:"center",
         alignItems:"center"
     }
